790c1a8484aa8a1e58359ef34a683ba8f62120d7
Natseol <jewelbox00@gmail.com>
2023-09-30 00:39:51 +0900

    테스트 : 웹페이지 상에서의 주사위 배열 그림 구현

--- src/main/webapp/script/main.js
+++ src/main/webapp/script/main.js
@@ -16,7 +16,7 @@ function inside() {
             console.log(data);
             console.log(data.player.inventory[0].name);
             console.log(data.script);
-            document.getElementsByClassName("script").innerHTML = "data.script";
+            document.getElementById("script").innerHTML = data.script;
             console.log("Name Value (inside fetch): " + nameValue);
             console.log("Age Value (inside fetch): " + ageValue);         
         })
@@ -24,6 +24,10 @@ function inside() {
             console.error('Error:', error);
     });
 };
+
+function ins1() {
+    document.getElementById("script").innerHTML = "변경";
+}
 /* 참고할 것
 // JSON 데이터 예제
 var json1 = '{"name": "John", "age": 30}';
@@ -39,16 +43,81 @@ console.log("Person 1 Age: " + person1.age);
 console.log("Person 2 Name: " + person2.name);
 console.log("Person 2 Age: " + person2.age);
 */
-	
-let isSelect = false;
-function changeColor() {
-    if (isSelect==true) {
-        document.getElementById("dice").src="image/dice1.png";
-        isSelect = false;
-    } else {
-        document.getElementById("dice").src="image/dicered1.png";
-        isSelect = true;
+
+
+
+// 0부터 9까지의 이미지 URL 배열을 정의합니다.
+const imagePaths = [
+    "image/dice10.png",
+    "image/dice1.png",
+    "http://192.168.0.35:5500/image/dice2.png",
+    "image/dice3.png",
+    "image/dice4.png",
+    "image/dice5.png",
+    "image/dice6.png",
+    "image/dice7.png",
+    "image/dice8.png",
+    "image/dice9.png"
+];
+
+const imageRedPaths = [
+    "image/dicered10.png",
+    "image/dicered1.png",
+    "http://192.168.0.35:5500/image/dicered2.png",
+    "image/dicered3.png",
+    "image/dicered4.png",
+    "image/dicered5.png",
+    "image/dicered6.png",
+    "image/dicered7.png",
+    "image/dicered8.png",
+    "image/dicered9.png"
+];
+
+const imageContainer = document.getElementById("image-container");
+
+const diceList = [2,3,5,6,2];
+
+for (let i = 0; i < diceList.length; i++) {
+
+    let imageElement = document.createElement("img");
+    imageElement.className = "dice-image";
+    imageElement.src = imagePaths[diceList[i]];
+    imageElement.addEventListener("click", function() {
+        console.log(imageElement.src);
+        console.log(imagePaths[diceList[i]]);
+        if (imageElement.src == imagePaths[diceList[i]]) {
+            console.log("검은색");
+            imageElement.src = imageRedPaths[diceList[i]];
+        } else if (imageElement.src == imageRedPaths[diceList[i]]) {
+            console.log("빨강");
+            imageElement.src = imagePaths[diceList[i]];
+        } else {
+            console.log("안됨");
+        }
+    });
+    
+    imageContainer.appendChild(imageElement);
+}
+
+function consolPrint() {
+    var container = document.getElementById("image-container");
+        // 컨테이너 내의 모든 이미지 요소 가져오기
+    var images = container.getElementsByTagName('img');
+
+    // 이미지를 찾는 반복문
+    for (var i = 0; i < images.length; i++) {
+        var currentImageSrc = images[i].src;
+        if (imageRedPaths.includes(images[i].src)) {
+            // 이미지가 배열에 있는 경우 해당 이미지에 대한 작업을 수행
+            console.log('이미지를 찾았습니다:', i);
+            // 추가 작업을 수행할 수 있습니다.
+        } else {
+            console.log('이미지가 없습니다:', i);
+        }
     }
 }
+
+
+
 
 	
